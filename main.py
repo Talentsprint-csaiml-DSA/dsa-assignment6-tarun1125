4b3703f0fb02ca33de740ac78f44a2b26e61177c
Tarun Gudapati <g.tarun25@gmail.com>
2024-11-24 12:18:46 +0530

    assignment6 submission

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def longest_box_sequence(boxes):
-    #ToDo 
-    #Write your code here.
-    
+    boxes.sort(key = (lambda box: (box[0], box[1], box[2])))
+
+    dp_array = [1] * len(boxes)
+
+    for i in range(1,len(boxes)):
+        for j in range(i):
+            if (boxes[j][0] < boxes[i][0] and
+                boxes[j][1] < boxes[i][1] and
+                boxes[j][2] < boxes[i][2]):
+                dp_array[i] = max(dp_array[i], dp_array[j] + 1)
+
+    return max(dp_array)  
